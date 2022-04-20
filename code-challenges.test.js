569bb8c14c3bdf4bf65eb2318c729b9a21d353f8
zmprunella <zane.prunella@gmail.com>
2022-04-19 19:21:24 -0700

    assessmnet updates

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,6 +1,6 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
-const { describe } = require("yargs");
+// const { describe } = require("yargs");
 
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
@@ -15,58 +15,49 @@ const { describe } = require("yargs");
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
-
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
 
-describe('adjustedArray', () => {
-
-    const colors1 = ["yellow", "blue", "pink", "green"]
-
-    it('takes in an array, removes the first item from the array and shuffles the remaining content.', () => {
-
-      expect(["purple", "blue", "green", "yellow", "pink"]).toEqual(expect.adjustedArray(expected));
-    });
-
-    it('does not match if received does not contain expected elements', () => {
-      expect(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]).not.toEqual(expect.adjustedArray(expected));
-
-    });
-  });
+// describe("theShuffler", () => {
+//     it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+//       const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+//       const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+//       expect(theShuffler(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+//       expect(theShuffler(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+//     })
+//   })
 
 
-// YError: Invalid second argument. Expected string but received function.
-// cant seem to get the yarn jest to read the test. 
-// error with a paragraph of yelllow text.
+  // FAIL  ./code-challenges.test.js
+//   theShuffler
+//   ✕ takes in an array, removes the first item from the array and shuffles the remaining content. (1 ms)
 
+// ● theShuffler › takes in an array, removes the first item from the array and shuffles the remaining content.
+
+//   ReferenceError: theShuffler is not defined
+
+// function - contentShuffler
+// parameter- array
+// ReferenceError: contentShuffler is not defined
+// slice first index
+// array.sort()) => (Math.rndom() >.5)   - Math.random() creates the random number, by subtracting .5 we can get a negative number or a positive number.
+
+
+// const theShuffler = (array) => {
+//   return array.slice(1).sort(() => Math.random() - .5)
+// }
 
 
 // b) Create the function that makes the test pass.
             
-
-// create a function
-// have function remove first item
-// have function shuffle remaining items
-// I don't know the correct syntax to connect all of these things but I think these are the right functions
-            
-
-const adjustedArray = (colors1, colors2) => {
-    function colors1.shift()
-    function colors2.shift
-    let nextArray = .map(value => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value)
-            
-}
-console.log(adjustedArray());
-
-            
+// PASS  ./code-challenges.test.js
+// theShuffler
+//   ✓ takes in an array, removes the first item from the array and shuffles the remaining content. (1 ms)          
 
 
 
@@ -76,33 +67,22 @@ console.log(adjustedArray());
 // a) Create a test with expect statements for each of the variables provided.
 
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+// const nums1 = [3, 56, 90, -8, 0, 23, 6]
+// // Expected output: [-8, 90]
+// const nums2 = [109, 5, 9, 67, 8, 24]
+// // Expected output: [5, 109]
 
 
-describe('arrayMinMax', () => {
 
-    const nums1 = [-8, 90];
-    const nums2 = [5, 109]
+// describe("minMax", () => {
+//     it("takes an array of numbers and returns an array of the minimum and maximum numbers in that order.", () => {
+//         const nums1 = [3, 56, 90, -8, 0, 23, 6]
+//         const nums2 = [109, 5, 9, 67, 8, 24]
+//       expect(minMax(nums1)).toEqual(expect.arrayContaining([-8, 90]))
+//       expect(minMax(nums2)).toEqual(expect.arrayContaining([5, 109]))
+//     })
+//   })
 
-    it('takes an array of numbers and returns an array of the minimum and maximum numbers in that order', () => {
-      expect([3, 56, 90, -8, 0, 23, 6]).toEqual(
-        expect.arrayMinMax(expected),
-      );
-    });
-
-    it('takes an array of numbers and returns an array of the minimum and maximum numbers in that order', () => {
-      expect([109, 5, 9, 67, 8, 24]).not.toEqual(
-        expect.arrayMinMax(expected),
-      );
-    });
-  });
-
-
-// Wasn't able to get the first test to read.
-// getting the same error but with this test now.
 
 
 // b) Create the function that makes the test pass.
@@ -112,15 +92,13 @@ describe('arrayMinMax', () => {
 // have the function filter the first and last number of new array into another new array.
 
 
-const arrayMinMax = (nums1,nums2) => {
-    function nums1
-    function nums2  
-    return array.pop 
-    return array.shift()
-}
-console.log(arrayMinMax());
+// const minMax = (array) => {
+//     return [Math.min(...array), Math.max(...array)]
+//   }
 
 
+//   PASS  ./code-challenges.test.js
+//   minMax
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
@@ -132,21 +110,17 @@ const testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
-describe('takes in two arrays as arguments and returns one array with no duplicate values', () => {
-    const expected = [3, 7, 10, 5, 4, 8, 2, 1]
+describe("twoArrays", () => {
+    it("takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.", () => {
+        const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+        const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+      expect(twoArrays(testArray1, testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+    })
+  })
 
-    it('takes in two arrays as arguments and returns one array with no duplicate values', () => {
-      expect([3, 7, 10, 5, 4, 3, 3]).toEqual(
-        expect.arrayAdd(expected),
-      );
-    });
-    it('takes in two arrays as arguments and returns one array with no duplicate values', () => {
-      expect([7, 8, 2, 3, 1, 5, 4]).not.toEqual(
-        expect.arrayAdd(expected),
-      );
-    });
-  });
 
+//   FAIL  ./code-challenges.test.js
+//   twoArrays
 
 // b) Create the function that makes the test pass.
 
@@ -155,18 +129,11 @@ describe('takes in two arrays as arguments and returns one array with no duplica
 // have function filter out any numbers that are in the new array more than once.
 
 
-arrayAdd(testArray1,testArray2)  
-     function arrayAdd(testArray1,testArray2){
-     let thirdArray = testArray1.concat(testArray2)
-     console.log(thirdArray)
+const twoArrays = (array1, array2) => {
+    array1 = [...new Set([...array1, ...array2])]
+    return array1
+  }
 
-     let uniqueArray = []
-     for(let i of thirdArray){
-         
-            if(uniqueArray.indexOf(i) === -1){
-                uniqueArray.push(i)
-            }
-     }
-     console.log(uniqueArray)
-}
 
+//   PASS  ./code-challenges.test.js
+//   twoArrays
--- interview-questions.md
+++ interview-questions.md
@@ -9,7 +9,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer: Can't quite remember what props are. I know they have something to do with class and components. I would have to do a litte more research on how they connect to the rest.
 
-  Researched answer: Similar to functions in JS, props are arguments passed into react compononets. 
+  Researched answer: Similar to functions in JS, props are arguments passed into react compononets. Read only components. Pass data between components. 
 
 
 
@@ -17,7 +17,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer: Would have to do a bit more research on DOM. I know that it has to do with React.
 
-  Researched answer: A DOM event in JS allows registration of different event handelers in a doccument.
+  Researched answer: A DOM event in JS allows registration of different event handelers in a doccument. User interactions in the browser; clicks the mouse, web pages loaded, image loaded, mouse moves, input feild is changed, html form is submitted, pressing a key
 
 
 
@@ -27,16 +27,18 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Researched answer: OOP is a computer programming model that organizes software design around data and objects rather than logic or functions. Functional programming cannot store data in objects, it can only transform it.
 
+  OOP- data can be stored in objects and behvior, funtional - behavior.
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
   Your answer: In Ruby a integer is a whole number where as a float is not a whole number
 
-  Researched answer: A float can be a rational or irrational number that contains fractional parts.
-    An integer is whole number.
+  Researched answer: A float can be a rational or irrational number that contains fractional parts. An integer is whole number.
 
+  Floats have decimal places. integers do not.
 
+  In JS they are just numbers, together. 
 
 5. Ruby has an implicit return. What does that mean?
 
@@ -46,14 +48,15 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 
 
+
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable: A type of variable that starts with an @ symbol.
+1. Instance Variable: A type of variable that belongs to a class.
 
-2. PostgreSQL: A highly stable database managemnt systme.
+2. PostgreSQL: A open source object relational database.
 
-3. Ruby on Rails: A server side web applicaiton framework written in Ruby.
+3. Ruby on Rails: A full-stack framwork created in the ruby programming language
 
 4. ORM: Object Relational Mapper. Automatically calls database
 
-5. Active Record: A Layer of ruby code that runs between database and logic code.
+5. Active Record: A Layer of ruby code that runs between database/model/business logic ruby on rails.
